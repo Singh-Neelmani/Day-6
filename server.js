141a991e364516f5a55869f95f33c5e8e98989ff
Singh-Neelmani <neelmani.singh@timesinternet.in>
2021-07-12 14:43:24 +0530

    server01

--- server.js
+++ server.js
@@ -11,6 +11,18 @@ server.get('/',function(request,response){
     console.log('value of accept header='+request.headers['cache-control']);
     console.log(request.response);
     response.send('Hello World');
+    const user = {
+        name: 'NEELMANI',
+        age: '22',
+        city: 'Varanasi',
+        DOB: '30/06/1999',
+        Company: 'Times',
+    }
+    console.log(user.name);
+    console.log(user.age);
+    console.log(user.city);
+    console.log(user.DOB);
+    console.log(user.Company);
 })
 server.listen(3000,function(){
     console.log('server started....');
